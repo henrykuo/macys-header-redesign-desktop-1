cd92b4af87ecf40205f55f20f3e77ff77ffef197
Henry Kuo <henry@henrykuo.com>
2019-11-18 11:55:56 -0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10465,6 +10465,7 @@ $__System.register('b', ['7', '9', 'a'], function (_export) {
 						if (menuState === 1) {
 							hide_menu();
 							hide_flyout();
+							hide_accountFlyout();
 						}
 						break;
 				}
